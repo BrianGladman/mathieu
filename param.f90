47dd9b0252a6c523b0e8bb94571f39b39ced9b9f
Brian Gladman <riemannic@gmail.com>
2021-03-31 16:12:05 +0100

    add missing param file

---
+++ param.f90
@@ -0,0 +1,6 @@
+module param
+    integer, parameter :: knd = selected_real_kind(16)
+    logical, parameter :: debug = .true.
+    logical, parameter :: warn = .true.
+    logical, parameter :: output = .false.
+end module param
